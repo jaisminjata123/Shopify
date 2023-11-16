2fe85afe1d3e6ed1ab9b00ac18e5e9fab06577b4
jaisminjata <jaisminjata195@gmail.com>
2023-11-16 21:40:09 +0530

    added payment method and shipping screen

--- fronted/src/screens/LoginScreen.jsx
+++ fronted/src/screens/LoginScreen.jsx
@@ -48,25 +48,22 @@ const LoginScreen = () => {
         <h1>Sign In</h1>
         <Form onSubmit ={submitHandler}>
             <Form.Group controlId='email' className="my-3">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e)=>setEmail(e.target.value)}>
-
-            </Form.Control>
-
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e)=>setEmail(e.target.value)}>
+              </Form.Control>
             </Form.Group>
-
 
             <Form.Group controlId='password' className="my-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e)=>setPassword(e.target.value)}>
-                
-            </Form.Control>
-
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e)=>setPassword(e.target.value)}>
+              </Form.Control>
             </Form.Group>
+
             <Button type='submit' variant='primary' className="my-2" disabled={isLoading}>
                 Sign In
             </Button>
             {isLoading&&<Loader/>}
+            
         </Form>
         <Row className="py-3">
             <Col>
--- fronted/src/screens/RegisterScreen.jsx
+++ fronted/src/screens/RegisterScreen.jsx
@@ -15,7 +15,6 @@ const RegisterScreen = () => {
   const [confirmPassword,setConfirmPassword]= useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const [register,{isLoading}]= useRegisterMutation();
   const {userInfo} = useSelector((state)=>state.auth)
   const {search} = useLocation();
@@ -53,42 +52,39 @@ const RegisterScreen = () => {
   };
   return (
     <FormContainer>
+
         <h1>Sign Un</h1>
+
         <Form onSubmit ={submitHandler}>
-        <Form.Group controlId='name' className="my-3">
+          <Form.Group controlId='name' className="my-3">
             <Form.Label>Name</Form.Label>
             <Form.Control type='text' placeholder='Enter name' value={name} onChange={(e)=>setName(e.target.value)}>
-
             </Form.Control>
+          </Form.Group>
 
-            </Form.Group>
-            <Form.Group controlId='email' className="my-3">
+          <Form.Group controlId='email' className="my-3">
             <Form.Label>Email Address</Form.Label>
             <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e)=>setEmail(e.target.value)}>
-
             </Form.Control>
+          </Form.Group>
 
-            </Form.Group>
-
-
-            <Form.Group controlId='password' className="my-3">
+          <Form.Group controlId='password' className="my-3">
             <Form.Label>Password</Form.Label>
             <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e)=>setPassword(e.target.value)}>
-                
             </Form.Control>
+          </Form.Group>
 
-            </Form.Group>
-            <Form.Group controlId='confirmPassword' className="my-3">
+          <Form.Group controlId='confirmPassword' className="my-3">
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control type='password' placeholder='Confirm password' value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}>
-                
             </Form.Control>
+          </Form.Group>
 
-            </Form.Group>
-            <Button type='submit' variant='primary' className="my-2" disabled={isLoading}>
+          <Button type='submit' variant='primary' className="my-2" disabled={isLoading}>
                 Register
-            </Button>
+          </Button>
             {isLoading&&<Loader/>}
+
         </Form>
         <Row className="py-3">
             <Col>
