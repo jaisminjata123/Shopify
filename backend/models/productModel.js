9e025e81743f58b3c06dde6cde97dbfe540b6557
jaisminjata <jaisminjata195@gmail.com>
2023-11-27 19:30:11 +0530

    added product editing from admin end

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -51,12 +51,10 @@ const productSchema = new mongoose.Schema({
     reviews:[reviewSchema],
     rating:{
         type:Number,
-        required:true,
         default:0
     },
     numReviews:{
         type:Number,
-        required:true,
         default:0
     },
     price:{
--- fronted/src/screens/admin/ProductEditScreen.jsx
+++ fronted/src/screens/admin/ProductEditScreen.jsx
@@ -38,6 +38,30 @@ const navigate = useNavigate();
 },[product]
     )
 
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const updatedProduct = {
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description,
+        };
+
+        const result = await updateProduct (updatedProduct);
+        if (result.error) {
+        
+        toast.error(result.error);
+        } else {
+        toast.success('Product updated');
+        navigate('/admin/productlist');
+            }
+        }
+
+
   return (
     <>
         <Link to='/admin/productlist' className="btn btn-light my-3">
@@ -48,16 +72,71 @@ const navigate = useNavigate();
         {loadingUpdate && <Loader />}
         {isLoading? <Loader /> : error ? <Message variant='danger'>
         {error}</Message> : (
-            <Form>
-            <Form.Group controlId='name'>
-                <Form.Label>Name</Form. Label>
-                <Form.Control
-                type='name'
-                placeholder='Enter name'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='name'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                    type='text'
+                    placeholder='Enter name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                 ></Form.Control>
                 </Form.Group>
+                <Form.Group controlId='price' className= 'my-2'>
+                    <Form.Label>Price</Form.Label>
+                    <Form.Control
+                    type='text'
+                    placeholder='Enter price'
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                ></Form.Control>
+                </Form.Group>
+                {/*Image Input Placeholder */}
+                
+                <Form.Group controlId='brand' className= 'my-2'>
+                    <Form.Label>Brand</Form.Label>
+                    <Form.Control
+                    type='number'
+                    placeholder='Enter brand'
+                    value={brand}
+                    onChange={(e) => setBrand(e.target.value)}
+                ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='countInStock' className= 'my-2'>
+                    <Form.Label>Count In Stock</Form.Label>
+                    <Form.Control
+                    type='text'
+                    placeholder='Enter countnStock'
+                    value={countInStock}
+                    onChange={(e) => setCountInStock(e.target.value)}
+                ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='category' className='my-2'>
+                    <Form.Label>Category</Form.Label>
+                    <Form.Control
+                    type='text'
+                    placeholder='Enter category'
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    ></Form.Control>
+                    </Form.Group>
+                <Form.Group controlId='description' className='my-2'>
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                type='text'
+                placeholder='Enter description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                ></Form.Control>
+                </Form.Group>
+                <Button type='submit' variant='primary' className= 'my-2'>
+                Update
+                </Button>
+
+
+
             </Form>
 
         )}
